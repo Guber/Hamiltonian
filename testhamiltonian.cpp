b6a662b60eeacd20a5c141bc2e4e30318ef94aa5
Emanuel Guberovic <emanuel.guberovic@fer.hr>
2017-04-10 19:08:54 +0200

    Update testhamiltonian.cpp

--- testhamiltonian.cpp
+++ testhamiltonian.cpp
@@ -1,14 +1,14 @@
-#include <stdio.h>
+e#include <stdio.h>
 #include <stdlib.h>
 
 /**
 Search for a hamiltonian cycle, than if there is no hamiltonian cycle, we can search for an almost hamiltonian path using dynamic programming and tabulation tehniques.
-This has been don for a colleuge class in graph theory. And together with my code I supply a few of test cases which we were given in the class.
+This has been don for a college class in graph theory. And together with my code I supply a few of test cases which we were given in the class.
 
 To run it you need to pass to it as an argumant the name of a file wich contains an adjacency matrix.
 So if we have a file test1.txt we call the compiled program  with a command to the cmd "testhamiltonian.exe test1.txt".
 
-File should in a first line state the number of vetices in the graph, followed by the adjacency matrix in the next lines.
+File should in a first line state the number of vertices in the graph, followed by the adjacency matrix in the next lines.
 
 For instance test1.txt could be:
 4
